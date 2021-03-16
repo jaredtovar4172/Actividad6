9cb9b34c0af48c87774a905d35123593536b79a5
jaredtovar4172 <80796166+jaredtovar4172@users.noreply.github.com>
2021-03-16 17:48:08 -0600

    Add files via upload

---
+++ Ej6_AreaTriangulo/Ej6_AreaTriangulo/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ej6_AreaTriangulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double c = double.Parse(textBox3.Text);
+            double p = (a + b + c) / 2;
+            double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            this.textBox4.Text = area.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox1.Focus();
+            textBox2.Clear();
+            textBox2.Focus();
+            textBox3.Clear();
+            textBox3.Focus();
+            textBox4.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult salir;
+            salir = MessageBox.Show("Deseas salir de la aplicacion?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (salir == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
